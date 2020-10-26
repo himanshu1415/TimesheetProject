b8d28a2df27b91abf7c7d12020c048a870d17cb4
himanshu dhaka <1415himanshu1415@gmail.com>
2020-10-26 16:59:44 +0530

    Update UnitTest1.cs

--- TimesheetNUnitTest/UnitTest1.cs
+++ TimesheetNUnitTest/UnitTest1.cs
@@ -6,25 +6,41 @@ using System.Linq;
 namespace TimesheetNUnitTest
 {
     public class Tests
-    { /*
-        public List<Employee> obj = new List<Employee>()
-        {  new Employee{   EmployeeId="", EmployeeName="",EmployeeEmail="",EmplyoeeGender="" }
-        };
+    { List<Employee> emp = new List<Employee>();
+        IQueryable<Employee> empdata;
+        Mock<DbSet<Employee>> mockSet;
+        Mock<TimesheetDBContext> empcontextmock;
         [SetUp]
         public void Setup()
         {
-            var details = obj.AsQueryable();
-            var mockset = new Mock<Employee>();
-            mockset.As<IQueryable<Employee>>().Setup(m => m.Provider).Returns(details.Provider);
-            mockset.As<IQueryable<Employee>>().Setup(m => m.Expression).Returns(details.Expression);
-            mockset.As<IQueryable<Employee>>().Setup(m => m.ElementType).Returns(details.ElementType);
-            mockset.As<IQueryable<Employee>>().Setup(m => m.GetEnumerator()).Returns(details.GetEnumerator());
+            emp = new List<Employee>()
+            { 
+              new Employee{EmployeeId = "FT111", EmployeeName="Himanshu", EmployeeEmail="1415himanshu1415@gmail.com",Gender="M"},
+              new Employee{EmployeeId = "CS101", EmployeeName="Shivam", EmployeeEmail="shivam@gmail.com",Gender="M"}
+            };
+            empdata = emp.AsQueryable();
+            mockSet = new Mock<DbSet<Employee>>();
+            mockSet.As<IQueryable<Employee>>().Setup(m => m.Provider).Returns(empdata.Provider);
+            mockSet.As<IQueryable<Employee>>().Setup(m => m.Expression).Returns(empdata.Expression);
+            mockSet.As<IQueryable<Employee>>().Setup(m => m.ElementType).Returns(empdata.ElementType);
+            mockSet.As<IQueryable<Employee>>().Setup(m => m.GetEnumerator()).Returns(empdata.GetEnumerator());
+            var p = new DbContextOptions<TimesheetDBContext>();
+            empcontextmock = new Mock<TimesheetDBContext>(p);
+            empcontextmock.Setup(x => x.Employee).Returns(mockSet.Object);
         }
 
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        } */
+         public void GetAllTest()
+            {
+                var emprepo = new TimesheetRepository(empcontextmock.Object);
+                var emplist = emprepo.GetAll();
+                Assert.AreEqual(2, emplist.Count());
+            }
+        public void GetEmployeeByIDTest()
+        {   string x = "CS101";
+            var emprepo = new TimesheetRepository(empcontextmock.Object);
+            var emplist = emprepo.GetById(x);
+            Assert.AreEqual(1, emplist.Count());
+        }
     }
 }
